456ffef5343f3bfcc696309696406da031a2cd23
Shivu Gururaj <gururajshivu@gmail.com>
2016-04-19 02:46:01 -0500

    Update AboutMe.java

--- profile App/app/src/main/java/com/shivu/myprofile/AboutMe.java
+++ profile App/app/src/main/java/com/shivu/myprofile/AboutMe.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class AboutMe extends Activity {
-
+	//about me!
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
